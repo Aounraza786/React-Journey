ca724d486c0769c4b72b6c3d6d42beaf6c569716
Aoun Raza <aoun.raza@systemsltd.com>
2026-02-23 19:32:21 +0500

    Day 04
    Day 04 topics Covered.
    Practice task: Dynamic todo list, Delete item ,Add item. Done Complete

---
+++ React-Cource/src/Components/ToDoListPractice.jsx
@@ -0,0 +1,23 @@
+function ToDoLists() {
+  const todos = [
+    { id: 1, text: "Learn React", completed: true },
+    { id: 2, text: "Practice map()", completed: false },
+    { id: 3, text: "Understand keys", completed: true },
+  ];
+  if (todos.length === 0) {
+    return <p>No List Found: </p>;
+  }
+
+  return (
+    <>
+      <h1>Todo List Practice</h1>
+      {todos.map((todo) => (
+        <li key={todo.id} style={{ background: "Blue", color: "white" }}>
+          {todo.text}
+          {" - "} {todo.completed ? "✅ Completed" : "⏳ Pending"}
+        </li>
+      ))}
+    </>
+  );
+}
+export default ToDoLists;
